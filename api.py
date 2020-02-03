e3f169b5ab0d646178c30ca69e90e908d483f472
02TheBear <Fredrik.Berzins@gmail.com>
2020-02-03 21:47:11 +0100

    removed api.py

--- api.py
+++
@@ -1,8 +0,0 @@
-import urllib3
-import json
-platform = input('Witch platform do you whant to search on?').lower()
-player = input('Witch player do you whant info about?').lower()
-url = f'https://r6tab.com/api/search.php?platform={platform}&search={player}'
-info_list = json.load(urllib3.###(url))
-print(info_list)
-
